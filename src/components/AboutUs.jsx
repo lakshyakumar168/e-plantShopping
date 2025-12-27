4a1f20a14aa0c77a6efbde2d84b1cda7250139c3
Lakshya Kumar <lakshyakumar168@gmail.com>
2025-12-27 16:18:44 +0530

    Add AboutUs component

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,18 @@
+function AboutUs() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery is your one-stop destination for beautiful and healthy
+        plants. We offer a wide variety of indoor and outdoor plants that bring
+        nature closer to your home.
+      </p>
+      <p>
+        Our mission is to provide high-quality plants at affordable prices while
+        promoting a greener and healthier lifestyle.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
